60dd8112d990aaf75b1cabc699be40dff5006268
prxg22 <prxg22@gmail.com>
2019-09-13 17:25:52 -0300

    Pushed sprint-23 by gh-pages-multi

---
+++ sprint-23/precache-manifest.0132c2c2724ed5835b2fb5d721b537ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5252887f2076feac3ecb014c5ab39887",
+    "url": "/sprint-review/index.html"
+  },
+  {
+    "revision": "7386bdfde51c65274d72",
+    "url": "/sprint-review/static/js/2.402e9e5c.chunk.js"
+  },
+  {
+    "revision": "b0bf538044fb0d772e69",
+    "url": "/sprint-review/static/js/main.2a4c4417.chunk.js"
+  },
+  {
+    "revision": "b7aee03eedf7e544970f",
+    "url": "/sprint-review/static/js/runtime~main.f0a4ed3e.js"
+  }
+]);
--- sprint-23/service-worker.js
+++ sprint-23/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8138594a65b91467d4e87e195ecfe31e.js"
+  "/sprint-review/precache-manifest.0132c2c2724ed5835b2fb5d721b537ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sprint-review/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
