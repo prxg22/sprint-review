dfd4069ee050339df672442ae72ac85de6c28ab8
prxg22 <prxg22@gmail.com>
2019-09-13 17:20:30 -0300

    Pushed sprint-23 by gh-pages-multi

---
+++ sprint-23/precache-manifest.8138594a65b91467d4e87e195ecfe31e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8149282bf6e7b0b58fb9fc04ec2a2797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d80d16325b4946f2b8b",
+    "url": "/static/js/2.998ca56d.chunk.js"
+  },
+  {
+    "revision": "ecc6ae0df136c5b7d798",
+    "url": "/static/js/main.9bf52c83.chunk.js"
+  },
+  {
+    "revision": "ae73e7ace87323c6c4a7",
+    "url": "/static/js/runtime~main.1e815694.js"
+  }
+]);
